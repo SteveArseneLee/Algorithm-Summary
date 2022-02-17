0c2b1a3dee6e7c0d7fc05854d4e02b072a36bc3c
SteveArseneLee <lclgood@naver.com>
2022-02-17 17:24:02 +0900

    'greedy'

--- Greedy/큰 수의법칙.py
+++ Greedy/큰 수의법칙.py
@@ -1,17 +1,32 @@
+# N,M,K = map(int, input().rstrip().split())
+# lst = list(map(int, input().split()))
+# lst.sort()
+# first = lst[-1]
+# second = lst[-2]
+# result = 0
+# while True:
+#     for i in range(K):
+#         if M == 0:
+#             break
+#         result += first
+#         M -= 1
+#     if M == 0:
+#         break
+#     result += second
+#     M -= 1
+# print(result)
+
+# ----------------------------------------
 N,M,K = map(int, input().rstrip().split())
 lst = list(map(int, input().split()))
 lst.sort()
 first = lst[-1]
 second = lst[-2]
+
+count = int(M / (K+1)) * K
+count += M % (K+1)
+
 result = 0
-while True:
-    for i in range(K):
-        if M == 0:
-            break
-        result += first
-        M -= 1
-    if M == 0:
-        break
-    result += second
-    M -= 1
+result += (count)*first # 가장 큰 수 더하기
+result += (M-count) * second # 두 번째로 큰 수 더하기
 print(result)
